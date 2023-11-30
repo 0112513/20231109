f05e2194bd2c557069251361caa14c06b5056e7d
0112513 <a0909805508@gmail.com>
2023-11-30 10:04:43 +0800

    first commit

---
+++ pra.js
@@ -0,0 +1,8 @@
+const spanText = document.getElementById("text")
+
+console.dir(spanText)
+spanText.innerText = "xxx"
+
+const aTest = document.getElementById("aTest");
+
+aTest.innerHTML = "<a href=https://www.youtube.com>cool</a>"
